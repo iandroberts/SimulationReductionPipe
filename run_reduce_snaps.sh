28618844aad3b5d5647a0913073778f3bc31b701
iandroberts <54645659+iandroberts@users.noreply.github.com>
2019-09-03 11:11:57 -0400

    Delete run_reduce_snaps.sh

--- run_reduce_snaps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for f in `cat snaplist.txt`
-do
-    echo $f
-    tail -n +64 /home/idroberts/mdpl2/halo_cats/hlist_0.${f}.list > /home/idroberts/mdpl2/processed_halo_cats/hlist_0.${f}_trim.dat
-done
